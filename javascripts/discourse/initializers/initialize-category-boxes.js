c14ff074e79e2ec07ebe93c72c22cb5b4921365b
Liberty Labs <github@libertylabs.de>
2026-09-10 12:04:26 +0200

    Update initialize-category-boxes.js

--- javascripts/discourse/initializers/initialize-category-boxes.js
+++ javascripts/discourse/initializers/initialize-category-boxes.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
-import CategoryBoxes from "../components/category-boxes";
+import CustomCategoryBoxes from "../components/custom-category-boxes";
 
 export default apiInitializer((api) => {
-  api.renderInOutlet("above-discovery-categories", ModernCategoryBoxes);
+  api.renderInOutlet("above-discovery-categories", CustomCategoryBoxes);
 });
