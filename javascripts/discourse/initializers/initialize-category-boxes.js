4781d5c516709e1ede8d448b274f76b89bebf2f4
Liberty Labs <github@libertylabs.de>
2026-09-12 21:28:56 +0200

    Update initialize-category-boxes.js

--- javascripts/discourse/initializers/initialize-category-boxes.js
+++ javascripts/discourse/initializers/initialize-category-boxes.js
@@ -1,18 +1,9 @@
 import { apiInitializer } from "discourse/lib/api";
-import LibertyCategoriesDisplay from "../components/liberty-categories-display";
+import LibertyCategoryBoxes from "../components/liberty-category-boxes";
 
 export default apiInitializer((api) => {
-  api.onPageChange(() => {
-    const route = api.container.lookup("service:router");
-
-    if (
-      route.currentRouteName === "discovery.index" ||
-      route.currentRouteName === "discovery.categories"
-    ) {
-      api.renderInOutlet(
-        "below-site-header",
-        LibertyCategoriesDisplay
-      );
-    }
-  });
+  api.renderInOutlet(
+    "below-site-header",
+    LibertyCategoryBoxes
+  );
 });
