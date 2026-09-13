55ced37d38d367b516b0f398d1182aaf5f41418d
Liberty Labs <github@libertylabs.de>
2026-09-13 14:56:11 +0200

    Update initialize-category-boxes.js

--- javascripts/discourse/initializers/initialize-category-boxes.js
+++ javascripts/discourse/initializers/initialize-category-boxes.js
@@ -1,168 +1,61 @@
-import Component from "@glimmer/component";
-import { service } from "@ember/service";
+import { apiInitializer } from "discourse/lib/api";
+import LibertyCategoryBoxes from "../components/liberty-category-boxes";
 
-import CategoryBoxes from "./category-boxes";
-import CustomCategoryBoxes from "./custom-category-boxes";
-import LibertyCategoryHeader from "./liberty-category-header";
+export default apiInitializer((api) => {
+  api.renderInOutlet(
+    "below-site-header",
+    LibertyCategoryBoxes
+  );
 
-export default class LibertyCategoryBoxes extends Component {
-  @service router;
-  @service site;
-
-  get currentUrl() {
-    return (
-      this.router.currentURL ||
-      window.location.pathname ||
-      ""
-    );
+  if (typeof ResizeObserver === "undefined") {
+    return;
   }
 
-  get pathname() {
-    return this.currentUrl.split("?")[0];
-  }
+  let observer = null;
+  let attached = false;
 
-  get isCategoriesPage() {
-    return (
-      this.pathname === "/categories" ||
-      this.pathname === "/categories/"
-    );
-  }
-
-  get isCategoryPage() {
-    return (
-      this.pathname === "/c" ||
-      this.pathname.startsWith("/c/")
-    );
-  }
-
-  get currentSlugPath() {
-    if (!this.isCategoryPage) {
-      return null;
+  const applyWidth = ([entry]) => {
+    if (!entry) {
+      return;
     }
 
-    const pathParts = this.pathname
-      .replace(/^\/c\//, "")
-      .split("/")
-      .filter(Boolean);
+    document.documentElement.style.setProperty(
+      "--main-outer-width",
+      `${Math.round(entry.contentRect.width)}px`
+    );
+  };
 
-    return pathParts
-      .filter((part) => !/^\d+$/.test(part))
-      .join("/");
-  }
-
-  get currentCategory() {
-    const slugPath = this.currentSlugPath;
-
-    if (!slugPath) {
-      return null;
+  const attachObserver = () => {
+    if (attached) {
+      return;
     }
 
-    return this.findCategory(
-      this.site.categories ?? [],
-      slugPath
+    const mainOutlet = document.getElementById(
+      "main-outlet-wrapper"
     );
-  }
 
-  findCategory(categories, slugPath) {
-    for (const category of categories) {
-      const categoryPath =
-        category.fullSlug ||
-        category.slugPath ||
-        category.slug;
-
-      if (
-        categoryPath === slugPath ||
-        category.slug === slugPath
-      ) {
-        return category;
-      }
-
-      const nestedCategory = this.findCategory(
-        category.subcategories ?? [],
-        slugPath
-      );
-
-      if (nestedCategory) {
-        return nestedCategory;
-      }
+    if (!mainOutlet) {
+      return;
     }
 
-    return null;
-  }
+    observer = new ResizeObserver(applyWidth);
+    observer.observe(mainOutlet);
+    attached = true;
+  };
 
-  get subcategories() {
-    return this.currentCategory?.subcategories ?? [];
-  }
+  const refreshObserver = () => {
+    observer?.disconnect();
+    observer = null;
+    attached = false;
 
-  get shouldDisplayHeader() {
-    return Boolean(
-      this.isCategoryPage &&
-        this.currentCategory
-    );
-  }
+    requestAnimationFrame(() => {
+      attachObserver();
+    });
+  };
 
-  get shouldDisplaySubcategories() {
-    return Boolean(
-      this.currentCategory?.show_subcategory_list &&
-        this.subcategories.length > 0
-    );
-  }
+  attachObserver();
 
-  get globalOutletArgs() {
-    return {
-      categories: this.site.categories ?? [],
-    };
-  }
-
-  get categoryBackgroundUrl() {
-    const category = this.currentCategory;
-
-    return (
-      category?.uploaded_background?.url ||
-      category?.uploaded_background ||
-      category?.background_url ||
-      null
-    );
-  }
-
-  get categoryBackgroundStyle() {
-    const url = this.categoryBackgroundUrl;
-
-    if (!url) {
-      return null;
-    }
-
-    const escapedUrl = url
-      .replace(/\\/g, "\\\\")
-      .replace(/"/g, '\\"');
-
-    return `--liberty-category-background: url("${escapedUrl}")`;
-  }
-
-  <template>
-    {{#if this.isCategoriesPage}}
-      <CustomCategoryBoxes
-        @outletArgs={{this.globalOutletArgs}}
-      />
-    {{else if this.shouldDisplayHeader}}
-      <div
-        class="liberty-category-area"
-        style={{this.categoryBackgroundStyle}}
-      >
-        <div class="liberty-category-content">
-          <LibertyCategoryHeader
-            @category={{this.currentCategory}}
-          />
-
-          {{#if this.shouldDisplaySubcategories}}
-            <div class="custom-category-boxes-container">
-              <CategoryBoxes
-                @categories={{this.subcategories}}
-              />
-            </div>
-          {{/if}}
-        </div>
-      </div>
-    {{/if}}
-  </template>
-}
+  api.onPageChange(() => {
+    refreshObserver();
+  });
+});
